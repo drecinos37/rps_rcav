ed3a9eb5b824bd81f0189854da4a53cf9956d58d
drecinos37 <drecinos@chicagobooth.edu>
2015-12-01 21:56:38 -0600

    Forgot to save

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+   root("game#user_plays_rock")   ## sets default page!
    get("/rock",     { :controller => "rps", :action => "rock" })
    get("/paper",    { :controller => "rps", :action => "paper" })
    get("/scissors", { :controller => "rps", :action => "scissors" })
